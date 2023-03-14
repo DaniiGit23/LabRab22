2cc45b0fa660dc5414abdceeed3bfab3e763c0d0
Daniil <kondratenko_danil.23@mail.ru>
2023-03-14 18:09:55 +0300

    Vvod peremennih

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <conio.h>
 #include <math.h>
+int main()
+{
+double a, b, c, D, x1, x2, x3;
+printf("Write a ");
+scanf_s("%lf", &a);
+printf("Write b ");
+scanf_s("%lf", &b);
+printf("Write c ");
+scanf_s("%lf", &c);
